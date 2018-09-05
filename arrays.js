69a06108c733a7518a563e50f0e82144a7a3d234
dehast59@gmail.com <dehast59@gmail.com>
2018-09-05 18:09:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,17 +1,14 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-//take 2 parameters, an array and an element and add to the beginning of the array.  this one should not modify the original array.
 function addElementToBeginningOfArray(array, element) {
   return[element, ...array]
 }
 
-//take 2 parameters, an array and an element, and add to the beginning of the array, this one should modify the original array.
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
 
-//take 2 parameters, an array and an element, and add to the end of the array, this should not modify the original array
 function addElementToEndOfArray(array, element) {
   return[...array, element]
 }
@@ -20,3 +17,4 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array = [...array, element]
   return array
 }
+
