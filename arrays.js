570fbcfbb6ad00f6a6dd34b33792c63c5d51073d
dehast59@gmail.com <dehast59@gmail.com>
2018-09-05 18:26:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,4 +27,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array
 }
 
-function removeElementFromBeginningOfArray(array)
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
